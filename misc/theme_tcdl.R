919f79bba063d1aa6e1c7a2f79db8fc4ce626ff7
Tim Lucas <timcdlucas@gmail.com>
2016-08-04 12:45:05 +0100

    More sorting figures to be signle column and readable.

--- misc/theme_tcdl.R
+++ misc/theme_tcdl.R
@@ -41,5 +41,29 @@ theme_set(theme_grey() + theme_tcdl)
 #theme_set(theme_grey() + theme_pres)
 
 
+theme_pub <-theme(text = element_text(family = "Helvetica", size = 16),
+        panel.grid.major.x = element_line(colour = "#ECECEC", size = 0.4, linetype = 1),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major.y = element_line(colour = "#ECECEC", size = 0.4, linetype = 1),
+        legend.title = element_text(size = 16, colour  =  "#8B8B8B"),
+        legend.key = element_rect(fill = "transparent", colour = NA),
+        legend.background = element_rect(fill = "transparent", colour = NA),
+        legend.key.size = unit(1, "lines"),
+        legend.text = element_text(size = 16, colour  =  "#8B8B8B"),
+        axis.text = element_text(color = "grey", size = 20, family  =  "Helvetica"),
+        axis.title = element_text(size = 20),
+        axis.title.y = element_text(vjust = 1.5),
+        axis.title.x = element_text(vjust = -1),
+        axis.line  =  element_line(colour  =  "grey"),
+        title = element_text(size = 22),
+        panel.border  =  element_blank(), 
+        plot.margin  =  unit(c(0.3, 0.1, 1, 1.3), "lines"),
+        panel.margin = unit(1, "lines"),
+        panel.background = element_rect(fill = "transparent", colour = NA), 
+        plot.background = element_rect(colour = NA, fill = 'transparent'),
+        strip.background = element_rect(colour = 'grey', fill = 'grey'),
+        strip.text = element_text(colour = 'white', family = 'Helvetica', size = 16)
+)
 
 
