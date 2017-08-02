d0170fa3cc9518cd4d81fcdde340c3b5a589b0c9
Tim Lucas <timcdlucas@gmail.com>
2017-08-02 10:43:51 +0100

    Combine SI and ms.

--- misc/KnitrOptions.R
+++ misc/KnitrOptions.R
@@ -1,6 +1,6 @@
 
 
-.libPaths(c('~/R/x86_64-pc-linux-gnu-library/', .libPaths()))
+#.libPaths(c('~/R/x86_64-pc-linux-gnu-library/', .libPaths()))
 
 # Default figure width
 propOfTextwidth = 0.8
@@ -9,16 +9,16 @@ opts_chunk$set(
   echo = FALSE, 
   error = FALSE,
   cache = TRUE, 
-  dev = c('postscript', 'pdf', 'png'),
+  #dev = c('postscript', 'pdf', 'png'),
   warning = FALSE,
   results = 'hide',
   message = FALSE,
-  fig.width = 6.45 * propOfTextwidth,
-  fig.height = 6.45 * propOfTextwidth * 0.5,
+  #fig.width = 6.45 * propOfTextwidth,
+  #fig.height = 6.45 * propOfTextwidth * 0.5,
   fig.align = 'center',
-  fig.pos = 't',
+  #fig.pos = 't',
   #fig.showtext = TRUE, # Embeds fonts.
-  out.width = paste0(propOfTextwidth,'\\textwidth'),
+  #out.width = paste0(propOfTextwidth,'\\textwidth'),
   dev.args=list(bg="transparent"),
   size = 'footnotesize',
   cache.extra = list(R.version)
